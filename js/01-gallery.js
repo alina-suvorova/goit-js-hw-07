8b8b0b5441a1f0611a9701ce865e3630a5cffeef
Alina <alinasuv@gmail.com>
2021-10-23 20:16:35 +0300

    Добавляет задание 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,30 +10,35 @@ imgContainer.addEventListener('click', onImgContainerClick)
 function createImgCardsMarkUp (images){
 return images.map ( ({preview, original, description}) => {
 return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
+    </a>
 </div>`
 }).join("");
 }
 
 function onImgContainerClick (event){
+    
+    event.preventDefault();
+    
     const isGalleryImgEl = event.target.classList.contains('gallery__image');
     if(!isGalleryImgEl){
         return;
     }
 
-    const activeLargeImg = event.target.dataset.source;
+    const activeImgUrl = getImageUrl(event.target);
 
     const instance = basicLightbox.create (
-            `<img src="${activeLargeImg}" width="800" height="600">`
-         )
-        
-        instance.show()
+            `<img src="${activeImgUrl}" width="800" height="600">`
+        )
+    instance.show()
 }
 
-
-
+function getImageUrl (img) {
+    return img.dataset.source;
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imgContainer = document.querySelector('.gallery');
+const imgMarkup = createImgCardsMarkUp(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+imgContainer.addEventListener('click', onImgContainerClick)
+
+function createImgCardsMarkUp (images){
+return images.map ( ({preview, original, description}) => {
+return `
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" data-source = "${original}" src="${preview}" alt="${description}" />
+</a>
+`
+}).join("");
+}
+
+function onImgContainerClick (event){
+    
+    event.preventDefault();
+
+    const isGalleryImgEl = event.target.classList.contains('gallery__image');
+    if(!isGalleryImgEl){
+        return;
+    }
+
+    // const activeImgUrl = getImageUrl(event.target);
+
+    new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+}
+
+// function getImageUrl (img){
+//     return img.dataset.source;
+// }
